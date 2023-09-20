61268e85f1508454764a34daeaf3dc9bb990163f
David Gonçalves Maia <david.goncalves@dcx.ufpb.br>
2023-09-20 10:37:46 -0300

    Exercício - Interface List

---
+++ Collections/List/Desafio/CarrinhoDeCompras.java
@@ -0,0 +1,40 @@
+package Collections.List.Desafio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Collections.List.ListaTarefas;
+
+public class CarrinhoDeCompras {
+    private List<Item> itens;
+
+
+    public CarrinhoDeCompras(){
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        itens.add(new Item(nome, preco, quantidade));
+    }
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList();
+        for(Item item : itens){
+            if(item.getNome().equalsIgnoreCase(nome)){
+                itensParaRemover.add(item);
+            }
+        }
+        itens.removeAll(itensParaRemover);
+    }
+    public void calcularValorTotal() {
+        double valorTotal = 0.0;
+        for (Item item : itens) {
+            valorTotal += item.getPreco() * item.getQuantidade();
+        }
+        System.out.println("Valor Total: " + valorTotal);
+    }
+
+    public void exibirItens(){
+        System.out.println(itens); 
+    }
+    
+}
---
+++ Collections/List/ListaTarefas.java
@@ -0,0 +1,37 @@
+package Collections.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> tarefas;
+
+
+    public ListaTarefas(){
+        this.tarefas = new ArrayList();
+    }
+    
+    public void adicionarTarefa(String descricao){
+        tarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList();
+        for (Tarefa tarefa : tarefas){
+            if(tarefa.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+        tarefas.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalDeTarefas(){
+        return tarefas.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefas);
+    }
+    
+}
